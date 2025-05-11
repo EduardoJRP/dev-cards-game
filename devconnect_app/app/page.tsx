78b1784fd02c76141abe8a19d16946dff50e59d5
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-05-11 15:44:54 -0500

    update: Created navbar

--- devconnect_app/app/page.tsx
+++ devconnect_app/app/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import React from "react";
+import "./globals.css";
+import NavBar from "../components/navbar";
+
 export default function Home() {
   return (
     <div>
-      Hello World
+      <NavBar />
     </div>
   );
 }
---
+++ devconnect_app/components/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+      {/* Add your navigation content here */}
+      <h1>Navigation Bar</h1>
+    </nav>
+  );
+}
