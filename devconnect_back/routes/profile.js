f2f2aab817ddc550256e7c414ae5e78465b02b21
LMigMa49 <luismcortesm@gmail.com>
2025-05-12 17:09:41 -0500

    Definición de endpoints de puntajes y perfil

--- devconnect_back/routes/profile.js
+++ devconnect_back/routes/profile.js
@@ -1,8 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send("Profile...");
+// logged user
+router
+  .route("/")
+  .get((req, res) => {
+    // query user
+    const myProfile = [
+      {
+        user_id: 125,
+        username: "luismd",
+        created_at: "1746100000",
+      },
+    ];
+    res.send(myProfile);
+  })
+  .put((req, res) => {
+    // edit user
+    const editedProfile = [
+      {
+        user_id: 125,
+        username: "luismdEdited",
+        created_at: "1746100000",
+      },
+    ];
+  });
+
+router.param("user_id", (req, res, next, user_id) => {
+  req.user_id = user_id;
+  next();
 });
 
 module.exports = router;
--- devconnect_back/routes/scores.js
+++ devconnect_back/routes/scores.js
@@ -1,8 +1,55 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send("Scores...");
+// general scoreboard
+router.get("/", (req, res) => {
+  // query total score by user
+  const usersScores = [
+    {
+      user_id: "123",
+      total_score: 5000,
+    },
+    {
+      user_id: "125",
+      total_score: 4230,
+    },
+  ];
+  res.send(usersScores);
+});
+
+router
+  .route("/:game_id")
+  .get((req, res) => {
+    // middleware: query scores by game_id
+    res.send([
+      {
+        user_id: "123",
+        total_score: 1000,
+      },
+      {
+        user_id: "125",
+        total_score: 800,
+      },
+    ]);
+  })
+  .patch((req, res) => {
+    // middleware: query game & user id and add/update new score
+    const newScore = [
+      {
+        user_id: "125",
+        total_score: 1200,
+      },
+    ];
+    res.send(newScore);
+  })
+  .delete((req, res) => {
+    const deletionResult = false;
+    res.send(deletionResult);
+  });
+
+router.param("user_id", (req, res, next, user_id) => {
+  req.user_id = user_id;
+  next();
 });
 
 module.exports = router;
