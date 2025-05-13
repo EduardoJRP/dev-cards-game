69a3e4a0bab15faea9416377b21937c74fd6b5e5
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-05-12 22:50:55 -0500

    update: improved the navbar

--- devconnect_app/components/navbar.tsx
+++ devconnect_app/components/navbar.tsx
@@ -2,9 +2,19 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav>
-      {/* Add your navigation content here */}
-      <h1>Navigation Bar</h1>
+    <nav className="flex items-center justify-between bg-purple-500 text-white p-4">
+      <h1 className="text-lg font-bold">Devconnect</h1>
+      <div className="flex space-x-4">
+        <a href="#home" className="hover:underline">
+          Home
+        </a>
+        <a href="#about" className="hover:underline">
+          About
+        </a>
+        <a href="#contact" className="hover:underline">
+          Contact
+        </a>
+      </div>
     </nav>
   );
 }
