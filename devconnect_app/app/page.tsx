c6bc263c1d4fd103455347af9fdb18f65b26d936
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-05-13 21:53:11 -0500

    update: created base for login and register page, added new changes to the main page

--- devconnect_app/app/page.tsx
+++ devconnect_app/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React from "react";
 import "./globals.css";
@@ -6,8 +6,15 @@ import NavBar from "../components/navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="page-background">
       <NavBar />
+      <div className="flex flex-col items-center justify-center min-h-screen ">
+        <h1 className="text-5xl font-semibold mb-8 text-white">Our Games</h1>
+        <main className="flex flex-row justify-between gap-4 w-full px-8">
+          <div className="flex-1 bg-purple-500 text-white text-center p-4 rounded shadow">hi</div>
+          <div className="flex-1 bg-purple-500 text-white text-center p-4 rounded shadow">hi2</div>
+        </main>
+      </div>
     </div>
   );
 }
