aeba07d554fd3fa059e9b540857c0ec0da15de31
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-05-15 18:51:50 -0500

    update: added control of routes with clerk

--- devconnect_app/app/page.tsx
+++ devconnect_app/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import React from "react";
+import { useRouter } from "next/navigation";
 import "./globals.css";
 import NavBar from "../components/navbar";
 import { SignedOut, SignedIn, SignInButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div
       className="page-background min-h-screen"
@@ -18,12 +21,12 @@ export default function Home() {
         <main className="flex flex-col items-center justify-center min-h-screen">
           <h1 className="text-5xl font-semibold mb-8 text-white">Our Games</h1>
           <div className="flex flex-row justify-between gap-4 w-full px-8">
-            <div className="flex-1 bg-purple-400 text-white text-center p-4 rounded shadow">
-              hi
-            </div>
-            <div className="flex-1 bg-purple-400 text-white text-center p-4 rounded shadow">
-              hi2
-            </div>
+            <button onClick={() => router.push("/game1")} className="flex-1 bg-purple-400 text-white text-center p-4 rounded shadow">
+              game1
+            </button>
+            <button onClick={() => router.push("/game2")} className="flex-1 bg-purple-400 text-white text-center p-4 rounded shadow">
+              game2
+            </button>
           </div>
         </main>
       </SignedIn>
