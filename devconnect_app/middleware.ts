50f8eb54545dadcf12a40fa9bd923a796fcc3e6e
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-05-14 21:28:07 -0500

    update: adding protected routes

--- devconnect_app/middleware.ts
+++ devconnect_app/middleware.ts
@@ -1,4 +1,4 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
@@ -10,3 +10,5 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+
+// const isProtectedRoute = createRouteMatcher(["/game1(.*)", "game2(.*)"]);
