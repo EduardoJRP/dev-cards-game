bcfea9b378c7fb5b512d911ce588c4716887cf4a
Eduardo Rojas <edu.rojas.paez@gmail.com>
2025-05-14 20:50:51 -0500

    update: added part of clerk, organizing the files and their access

--- devconnect_app/app/layout.tsx
+++ devconnect_app/app/layout.tsx
@@ -1,6 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Geist, Geist_Mono } from 'next/font/google'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
 export const metadata: Metadata = {
   title: "DevConnect",
   description: "App for developers to connect with each other",
@@ -12,10 +32,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton/>
+              <SignUpButton/>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
