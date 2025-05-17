0abf82c4376964f7865f487c96b74c616124b036
John <johnelvis963@gmail.com>
2025-05-16 21:44:27 -0500

    Refactorizar el componente ColorSelect para mejorar el manejo de propiedades y la interfaz de usuario de selección de color

--- devconnect_app/components/shared/ColorSelect.tsx
+++ devconnect_app/components/shared/ColorSelect.tsx
@@ -1,64 +1,129 @@
 'use client'
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Check } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import { hexToRgba } from '@/utils/transform';
 
 type ColorOption = {
-   value: string,
-   color: string,
+  value: string
+  color: string
 }
 
 interface Props {
-   className?: string,
-   color: 'primary' | 'secondary' | 'accent',
-   defaultValue?: string,
-   options: ColorOption[],
-   onChange?: (value: string) => void,
-   activeError?: boolean,
+  className?: string
+  color: 'primary' | 'secondary' | 'accent'
+  name?: string
+  error?: boolean
+  success?: boolean
+  required?: boolean
+  defaultValue?: string
+  options: ColorOption[]
+  activeError?: boolean
+  label?: string
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
 }
 
 export const ColorSelect = ({
-   className,
-   color,
-   defaultValue,
-   options,
-   onChange
+  className,
+  color,
+  name,
+  defaultValue,
+  options,
+  onChange,
+  label,
+  required = false,
+  error = false,
+  success = false,
 }: Props) => {
+  const [selectedColor, setSelectedColor] = useState<string>(
+    defaultValue || options[0]?.value || ''
+  )
 
-   const [selectedColor, setSelectedColor] = useState<string>(defaultValue || options[0]?.value || '');
+  useEffect(() => {
+    if (onChange && name) {
+      const event = {
+        target: { name, value: selectedColor }
+      } as unknown as ChangeEvent<HTMLInputElement>
 
-   const handleColorSelect = (value: string) => {
-      setSelectedColor(value)
-      if (onChange) onChange(value);
-   }
+      onChange(event)
+    }
+  }, [])
 
-   const styleColor = {
-      'primary': 'border border-primary dark:border-primary-dark',
-      'secondary': 'border border-secondary dark:border-secondary-dark',
-      'accent': 'border border-accent dark:border-accent-dark',
-   }
 
-   return (
-      <div className={cn("flex items-center gap-2 rounded p-2 border border-primary", styleColor[color as keyof typeof styleColor], className)}>
-         {options.map((option) => (
-            <button
-               key={option.value}
-               style={{ backgroundColor: option.color }}
-               onClick={() => handleColorSelect(option.value)}
-               aria-label={`Select ${option.value} color`}
-               aria-pressed={selectedColor === option.value}
-               className={cn(
-                  "h-10 w-10 rounded-full flex items-center justify-center transition-all",
-                  "focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50",
-                  "hover:scale-110",
-               )}
-            >
-               {
-                  selectedColor === option.value
-                  && <Check className="h-5 w-5 text-white" />
-               }
-            </button>
-         ))}
+  const handleColorSelect = (value: string) => {
+    setSelectedColor(value)
+
+    if (onChange && name) {
+      const event = {
+        target: { name, value }
+      } as unknown as ChangeEvent<HTMLInputElement>
+
+      onChange(event)
+    }
+  }
+
+  const styleColor = {
+    primary: 'hover:border-primary dark:border-primary-dark',
+    secondary: 'border-secondary dark:border-secondary-dark',
+    accent: 'border-accent dark:border-accent-dark',
+  }
+  const state = cn({
+    'focus-within:ring-1 focus-within:ring-red-500 border-red-500': error,
+    'focus-within:ring-1 focus-within:ring-green-500 border-green-500': success,
+    // 'focus-within:ring-1 focus-within:ring-primary': !error && !success,
+
+    // 'focus-within:ring-1 focus-within:ring-primary/50 focus-within:outline-primary/20 outline-transparent outline-2 outline-offset-1 transition-all': !error && !success,
+  })
+
+  return (
+    <div className="space-y-1">
+      {label && (
+        <label
+          htmlFor={name}
+          className="text-sm font-medium text-gray-700 dark:text-gray-300 block"
+        >
+          {label}
+        </label>
+      )}
+
+      <input
+        type="hidden"
+        name={name}
+        value={selectedColor}
+        required={required}
+      />
+
+      <div
+        className={cn(
+          'transition-colors flex items-center gap-2 border border-gray-300 cursor-pointer rounded-lg p-2',
+          styleColor[color],
+          state,
+          className
+        )}
+      >
+        {options.map((option) => (
+          <button
+            type="button"
+            key={option.value}
+            onClick={() => handleColorSelect(option.value)}
+            aria-label={`Select ${option.value} color`}
+            aria-pressed={selectedColor === option.value}
+            style={{
+              backgroundColor: option.color,
+              outline: selectedColor === option.value
+                ? `2px solid ${hexToRgba(option.color, 0.5)}`
+                : 'none',
+              outlineOffset: '0px'
+            }}
+            className={cn(
+              'h-8 w-8 transition-all flex items-center justify-center rounded-full',
+              'hover:scale-110'
+            )}
+          >
+            {selectedColor === option.value && <Check className="h-5 w-5 text-white" />}
+          </button>
+        ))}
       </div>
-   )
+    </div>
+  )
 }
